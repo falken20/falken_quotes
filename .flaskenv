314cf73920bd0c399ebd74cc95a81e958387b8b8
Richi <r.rodriguez@bbva.com>
2022-11-26 20:38:13 +0100

    Update app.yaml and some fix in the UX

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,5 @@
 #FLASK_APP=./falken_quotes/main.py
-FLASK_APP=./main.py
+FLASK_APP=./falken_quotes/main.py
 FLASK_ENV=development
 FLASK_DEBUG=1
 #FLASK_RUN_PORT=8080
--- main.py
+++
@@ -1,8 +0,0 @@
-# by Richi Rod AKA @richionline / falken20
-
-from falken_quotes.main import app
-
-
-if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run()
